dc77e9ea89c26b045f040f9d9677ad5e4269a498
achyutjadhav5110-creator <achyutjadhav5110@gmail.com>
2026-04-28 19:27:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showLove() {
+  alert("I Love You Forever ❤️");
+}
+
+function moveNo(btn) {
+  btn.style.position = "absolute";
+  btn.style.left = Math.random() * window.innerWidth + "px";
+  btn.style.top = Math.random() * window.innerHeight + "px";
+}
